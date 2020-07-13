2b3dff10484d1d32c03e4965049d4d6ce00e3140
dark00shadow <twistcharlievlogs@gmail.com>
2020-07-13 20:31:34 +0100

    Update __init__.py

--- RectangleCollision/__init__.py
+++ RectangleCollision/__init__.py
@@ -1,4 +1,7 @@
 class collision():
-    def rectangle(obj1x,obj1y,obj2x,obj2y,obj1w=32,obj1h=32,obj2w=32,obj2h=32):
-         if obj1x < obj2x + obj2w and obj1x + obj1w > obj2x and obj1y < obj2y + obj2h and obj1y + obj1h > obj2y:
-            return True
+    def rectangle(x, y, target_x, target_y, width=32, height=32, target_width=32, target_height=32):
+
+        if (x >= target_x and x < (target_x + target_width)) or ((x + width) >= target_x and (x + width) < (target_x + target_width)):
+            if (y >= target_y and y < (target_y + target_height)) or ((y + height) >= target_y and (y + height) < (target_y + target_height)):
+                return True
+        return False

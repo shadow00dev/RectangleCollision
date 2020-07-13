cdfeb305c4102d85a4b1cd6216f9bdb8315af9fc
dark00shadow <twistcharlievlogs@gmail.com>
2020-07-13 20:47:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
   name='RectangleCollision',
-  version='0.0.0.5',
+  version='0.0.0.6',
   description='A basic collision module',
   long_description=open('desc.txt').read(),
   url='https://github.com/dark00shadow/RectangleCollision',
